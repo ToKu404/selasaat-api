428c7079c10575b46eea34c17c1025d14234847d
toku404 <phantom26isn@gmail.com>
2025-08-29 21:21:44 +0800

    db

--- config/database.py
+++ config/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 DATABASE_URL = "mysql+aiomysql://selasaat:s4n%402ooImysql@103.103.20.118:3306/selasaat"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
-SessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+SessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False, pool_pre_ping=True)
 Base = declarative_base()
 
 async def get_db():
